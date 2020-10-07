6687c36d0bb99d470f76f2d81133bcffaa49c054
nietoarranz <enriquenietoarranz@gmail.com>
2020-10-07 14:40:26 +0200

    changes quique

---
+++ Assets/Scripts/ClickCard.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class ClickCard : MonoBehaviour, IPointerClickHandler
+{
+    //Detect if a click occurs
+    public void OnPointerClick(PointerEventData pointerEventData)
+    {
+		SceneManager.LoadScene("GameScene");
+	}
+}
